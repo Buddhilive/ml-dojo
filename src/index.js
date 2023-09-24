59e1fdf725974c634586744f675f1c4fab3d087e
Buddhi Kavindra <visitbudkavin@gmail.com>
2023-09-24 11:14:25 +0530

    initialize web project with SCSS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("Namo Buddhaya!");
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html');
+});
+
+app.listen(3000, () => {
+  console.log('Server is listening on port 3000');
+});
